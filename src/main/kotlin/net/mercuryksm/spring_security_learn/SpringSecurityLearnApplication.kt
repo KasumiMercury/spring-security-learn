e85d8ada92ebd90ebd9761d335736581076b55b6
KasumiMercury <naoki_shira@outlook.jp>
2025-07-27 21:12:39 +0900

    init

---
+++ src/main/kotlin/net/mercuryksm/spring_security_learn/SpringSecurityLearnApplication.kt
@@ -0,0 +1,11 @@
+package net.mercuryksm.spring_security_learn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityLearnApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityLearnApplication>(*args)
+}
---
+++ src/test/kotlin/net/mercuryksm/spring_security_learn/SpringSecurityLearnApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.mercuryksm.spring_security_learn
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityLearnApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
